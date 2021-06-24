f3b3e2520b6d996e583c654d0e313841036bdf63
Andre Soares <andregsilv@gmail.com>
2021-06-24 11:49:09 -0300

    mocks

--- gen.go
+++ gen.go
@@ -1,4 +1,4 @@
 package main
 
-//go:generate mockgen -package=bench -destination=lib/bench/mock_bench_test.go     github.com/mniak/bench/lib/bench     FileFinder,ToolchainProducer,Builder,Tester
+//go:generate mockgen -package=mocks -destination=internal/mocks/bench.go     github.com/mniak/bench/lib/bench     FileFinder,ToolchainProducer,Builder,Tester
 //go:generate mockgen -package=mocks -destination=internal/mocks/toolchain.go github.com/mniak/bench/lib/toolchain Toolchain
