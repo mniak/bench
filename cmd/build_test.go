8d23ac518f7b0f9a0647e6c6906f97659e68409c
Andre Soares <andregsilv@gmail.com>
2021-08-02 09:10:02 -0300

    build working

--- cmd/build_test.go
+++ cmd/build_test.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"math/rand"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 
@@ -10,29 +14,42 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Build_WithFolderName(t *testing.T) {
-	const SOURCE_CODE = `
+func Test_Build_WithFolderName_AndRunToCheckIfBuiltCorrectly(t *testing.T) {
+	expectedExitCode := rand.Intn(200) + 1
+	sourceCode := fmt.Sprintf(`
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-void main() {
-    string name;
-    cin >> name;
-    cout << "BUILD: ";
-    cout << "Hello, " << name << endl;
-}`
+int main() {
+    cout << "Hello, world (%d)!";
+	return %d;
+}`, expectedExitCode, expectedExitCode)
 
 	tempdir, err := os.MkdirTemp("", "build_*")
 	require.NoError(t, err)
 	defer os.RemoveAll(tempdir)
 
-	err = os.WriteFile(filepath.Join(tempdir, "main.cpp"), []byte(SOURCE_CODE), 0644)
+	inputPath := filepath.Join(tempdir, "main.cpp")
+	err = os.WriteFile(inputPath, []byte(sourceCode), 0644)
 	require.NoError(t, err)
 
 	err = Build(tempdir)
 	require.NoError(t, err)
 
-	assert.FileExists(t, filepath.Join(tempdir, "main"+domain.OSBinaryExtension))
+	outputPath := filepath.Join(tempdir, "main"+domain.OSBinaryExtension)
+	assert.FileExists(t, outputPath)
+
+	var stdout bytes.Buffer
+	cmd := exec.Command(outputPath)
+	cmd.Stdout = &stdout
+
+	err = cmd.Run()
+	require.IsType(t, &exec.ExitError{}, err)
+	_, ok := err.(*exec.ExitError)
+	require.True(t, ok)
+	require.Equal(t, expectedExitCode, cmd.ProcessState.ExitCode())
+
+	require.Equal(t, fmt.Sprintf("Hello, world (%d)!", expectedExitCode), stdout.String())
 }
